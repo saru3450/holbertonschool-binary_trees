9dc69d44bdf8e1b4431ededba6c88fa6179218c5
Saru3450 <9971@holbertonstudents.com>
2024-12-12 15:05:09 +0000

    16. Is perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -28,8 +28,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 /**
  * binary_tree_is_perfect - function to check if tree is perfect or not
  * @tree: pointer to the root node
- * Return: 1 if it success or 0 
-*/
+ * Return: 1 if it success or 0
+ */
 
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {

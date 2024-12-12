bab1b712563b94fbf17f67ebeb0819f1acf54393
Saru3450 <9971@holbertonstudents.com>
2024-12-12 10:07:03 +0000

    13. Nodes

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -8,10 +8,16 @@
 
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
+	size_t nodes = 0;
+	size_t tree_nodes = 0;
+
 	if (tree == NULL)
 		return (0);
 
-	if (tree->left == NULL || tree->right == NULL)
-		return (1);
-	return (binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right) + 1);
+	if (!tree->left && !tree->right)
+		return (0);
+
+	nodes = binary_tree_nodes(tree->left);
+	tree_nodes = binary_tree_nodes(tree->right);
+	return (nodes + tree_nodes + 1);
 }
